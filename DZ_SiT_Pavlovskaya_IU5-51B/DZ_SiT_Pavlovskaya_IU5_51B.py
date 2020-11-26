04367b186426b3540d2d3c519ec6cbf7f300a7af
Анастасия Павловская <paa4851@gmail.com>
2020-11-26 12:55:10 +0300

    Corrected

--- DZ_SiT_Pavlovskaya_IU5-51B/DZ_SiT_Pavlovskaya_IU5_51B.py
+++ DZ_SiT_Pavlovskaya_IU5-51B/DZ_SiT_Pavlovskaya_IU5_51B.py
@@ -61,7 +61,7 @@ def CreateTable(v1):
     n0 = [0]*15
     Combinations = [0]*15
     all_errors = []
-    for i in range (0,14):
+    for i in range (0,15):
         e1[i] = 1
         all_errors = list(multiset_permutations(e1))
         for j in range (len(all_errors)):
@@ -75,7 +75,7 @@ def CreateTable(v1):
         Combinations[i]=int(factorial(15)/(factorial(i+1)*factorial(15-(i+1))))
         C0[i] = n0[i]/Combinations[i]
         C0[i] =float('{:.7f}'.format(C0[i]))
-    Combinations[14]=int(factorial(15)/(factorial(15)*factorial(0)))
+
     i1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
     table = PrettyTable()
 
